bd1fb12eee81243209ea89dc1d1ebd31f9616046
kotcity <kotcity@zoho.com>
2018-01-12 18:47:47 -0500

    zoomin kinda

--- src/main/kotlin/kotcity/ui/GameFrame.kt
+++ src/main/kotlin/kotcity/ui/GameFrame.kt
@@ -3,8 +3,10 @@ package kotcity.ui
 import javafx.animation.AnimationTimer
 import javafx.application.Application
 import javafx.scene.canvas.GraphicsContext
+import javafx.scene.control.Accordion
 import javafx.scene.control.ScrollPane
 import javafx.scene.control.SplitPane
+import javafx.scene.control.TitledPane
 import javafx.scene.layout.BorderPane
 import javafx.scene.paint.Color
 import javafx.stage.Stage
@@ -21,17 +23,24 @@ class GameFrame : View(), CanvasFitter {
     override val root: BorderPane by fxml("/GameFrame.fxml")
     private val canvas = ResizableCanvas()
     private val canvasPane: ScrollPane by fxid("canvasPane")
+    private val accordion: Accordion by fxid()
+    private val basicPane: TitledPane by fxid()
 
     var ticks = 0
+    var zoom = 1.0
 
     lateinit var _map: CityMap
 
     fun setMap(map: CityMap) {
         this._map = map
         // gotta resize the component now...
-        canvas.width = map.width.toDouble()
-        canvas.height = map.height.toDouble()
+        resizeCanvas()
         println("Map has been set to: $_map. Size is ${canvas.width}x${canvas.height}")
+    }
+
+    fun resizeCanvas() {
+        canvas.width = _map.width.toDouble() * zoom
+        canvas.height = _map.height.toDouble() * zoom
     }
 
     fun getMap(): CityMap {
@@ -39,12 +48,18 @@ class GameFrame : View(), CanvasFitter {
     }
 
     fun drawMap(gc: GraphicsContext) {
-        if (this._map == null) {
-            return
-        }
-
         // we got that map...
         val (xRange, yRange) = canvasPane.visibleArea()
+
+        val zoomedX = IntRange(
+                (xRange.first.toDouble() / zoom).toInt(),
+                (xRange.last.toDouble() / zoom).toInt()
+        )
+
+        val zoomedY = IntRange(
+                (yRange.first.toDouble() / zoom).toInt(),
+                (yRange.last.toDouble() / zoom).toInt()
+        )
 
         for (x in xRange) {
             for (y in yRange) {
@@ -54,9 +69,21 @@ class GameFrame : View(), CanvasFitter {
                 } else {
                     gc.fill = Color.LIGHTBLUE
                 }
-                gc.fillRect(x.toDouble(), y.toDouble(), 1.0, 1.0)
+                gc.fillRect(x.toDouble() * zoom, y.toDouble() * zoom, zoom, zoom)
             }
         }
+    }
+
+    fun zoomOut() {
+        if (zoom > 1) {
+            zoom -= 1
+            resizeCanvas()
+        }
+    }
+
+    fun zoomIn() {
+        zoom += 1
+        resizeCanvas()
     }
 
     init {
@@ -66,17 +93,30 @@ class GameFrame : View(), CanvasFitter {
         // fitCanvasToPane(canvas, canvasPane)
         canvasPane.content = canvas
 
+        with(canvas) {
+            this.setOnScroll {  scrollEvent ->
+                // println("We are scrolling: $scrollEvent")
+                if (scrollEvent.deltaY < 0) {
+                    println("Zoom out!")
+                    zoomOut()
+                } else if (scrollEvent.deltaY > 0) {
+                    println("Zoom in!")
+                    zoomIn()
+                }
+            }
+        }
+
+        accordion.expandedPane = basicPane
+
         val timer = object : AnimationTimer() {
             override fun handle(now: Long) {
-                if (ticks == 5) {
+                if (ticks == 20) {
                     drawMap(canvas.graphicsContext2D)
                     ticks = 0
                 }
                 ticks++
             }
-
         }
-
         timer.start()
     }
 
