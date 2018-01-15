e8628c8e8fa2c06db33cd3f838e51804917cae92
kotcity <kotcity@zoho.com>
2018-01-14 23:15:51 -0500

    fixed up zoomable game frame renderer...

--- src/main/kotlin/kotcity/ui/GameFrame.kt
+++ src/main/kotlin/kotcity/ui/GameFrame.kt
@@ -5,7 +5,6 @@ import javafx.application.Application
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.control.Accordion
 import javafx.scene.control.ScrollBar
-import javafx.scene.control.ScrollPane
 import javafx.scene.control.TitledPane
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.VBox
@@ -29,90 +28,127 @@ class GameFrame : View(), CanvasFitter {
     private val verticalScroll: ScrollBar by fxid()
     private val horizontalScroll: ScrollBar by fxid()
 
-    var offsetX = 0.0
-    var offsetY = 0.0
+    var blockOffsetX = 0.0
+    var blockOffsetY = 0.0
 
     var ticks = 0
     var zoom = 1.0
+
+    // each block should = 10 meters, square...
+    // 64 pixels = 10 meters
+    fun blockSize(): Double {
+        // return (this.zoom * 64)
+        return when (zoom) {
+            1.0 -> 4.0
+            2.0 -> 8.0
+            3.0 -> 16.0
+            4.0 -> 32.0
+            5.0 -> 64.0
+            else -> 64.0
+        }
+    }
 
     lateinit var _map: CityMap
 
     fun setMap(map: CityMap) {
         this._map = map
         // gotta resize the component now...
-        resizeCanvas()
+        setScrollbarSizes()
+        setCanvasSize()
         println("Map has been set to: $_map. Size is ${canvas.width}x${canvas.height}")
     }
 
-    fun resizeCanvas() {
-        // BUG: we cannot have a large canvas!!!
-        canvas.width = _map.width.toDouble() * zoom
-        canvas.height = _map.height.toDouble() * zoom
+    private fun setCanvasSize() {
+        println("map size is: ${this._map.width},${this._map.height}")
+        println("Canvas pane size is: ${canvasPane.width},${canvasPane.height}")
+        canvas.prefHeight(canvasPane.height - 20)
+        canvas.prefWidth(canvasPane.width - 20)
+        AnchorPane.setTopAnchor(canvas, 0.0)
+        AnchorPane.setBottomAnchor(canvas, 20.0)
+        AnchorPane.setLeftAnchor(canvas, 0.0)
+        AnchorPane.setRightAnchor(canvas, 20.0)
+    }
+
+    private fun setScrollbarSizes() {
+
+        // TODO: don't let us scroll off the edge...
+
+        horizontalScroll.min = 0.0
+        horizontalScroll.max = getMap().width.toDouble()
+
+        verticalScroll.min = 0.0
+        verticalScroll.max = getMap().height.toDouble()
+
+        println("Horizontal set to: ${horizontalScroll.max}")
+        println("Vertical set to: ${verticalScroll.max}")
+       
     }
 
     fun getMap(): CityMap {
         return this._map
     }
 
+    private fun getVisibleBlocks(): Pair<IntRange, IntRange> {
+        val startBlockX = blockOffsetX.toInt()
+        val startBlockY = blockOffsetY.toInt()
+        var endBlockX = startBlockX+canvasBlockWidth()
+        var endBlockY = startBlockY+canvasBlockHeight()
+
+        if (endBlockX > getMap().width) {
+            endBlockX = getMap().width
+        }
+
+        if (endBlockY > getMap().height) {
+            endBlockY = getMap().height
+        }
+
+        return Pair(startBlockX..endBlockX, startBlockY..endBlockY)
+    }
+
+    private fun canvasBlockHeight() = (canvas.height / blockSize()).toInt()
+
+    private fun canvasBlockWidth() = (canvas.width / blockSize()).toInt()
+
+
     fun drawMap(gc: GraphicsContext) {
         // we got that map...
-        val (xRange, yRange) = visibleArea()
+        val (xRange, yRange) = getVisibleBlocks()
+        println("Visible blocks: ${getVisibleBlocks()}")
 
-        val zoomedX = IntRange(
-                (xRange.first.toDouble() / zoom).toInt(),
-                (xRange.last.toDouble() / zoom).toInt()
-        )
-
-        val zoomedY = IntRange(
-                (yRange.first.toDouble() / zoom).toInt(),
-                (yRange.last.toDouble() / zoom).toInt()
-        )
-
-        for (x in zoomedX) {
-            for (y in zoomedY) {
+        for (x in xRange) {
+            for (y in yRange) {
                 val tile = getMap().groundLayer[MapCoordinate(x, y)]
                 if (tile == GroundTile.GROUND) {
                     gc.fill = Color.LIGHTGOLDENRODYELLOW
                 } else {
                     gc.fill = Color.LIGHTBLUE
                 }
-                gc.fillRect(x.toDouble() * zoom, y.toDouble() * zoom, zoom, zoom)
+
+                val xi = xRange.indexOf(x)
+                val yi = yRange.indexOf(y)
+
+                if (x == xRange.first && y == yRange.first) {
+//                    println("i want to paint block: ${x}, ${y}")
+//                    println("starting at ${xi}, ${yi}")
+                }
+
+                gc.fillRect(
+                        xi * blockSize(),
+                        yi * blockSize(),
+                        blockSize(), blockSize()
+                )
             }
         }
-    }
-
-    private fun visibleArea(): Pair<IntRange, IntRange> {
-        // OK... what the FUUUUCK
-
-        // scrollpane does this...
-        // i am too drunk to code this now...
-        // val hValue = this.hvalue
-        // val vValue = this.vvalue
-        // val width = this.viewportBoundsProperty().get().width
-        // val height = this.viewportBoundsProperty().get().height
-        //
-        // val startX = (this.content.boundsInParent.width - width) * hValue
-        // val startY = (this.content.boundsInParent.height - height) * vValue
-        // val endX = startX + width
-        // val endY = startY + height
-        //
-        // val xRange = startX.toInt()..endX.toInt()
-        // val yRange = startY.toInt()..endY.toInt()
-        // return Pair(xRange, yRange)
-
-        return Pair(0..100, 0..100)
     }
 
     fun zoomOut() {
         if (zoom > 1) {
             zoom -= 1
-            resizeCanvas()
         }
     }
 
     fun zoomIn() {
         zoom += 1
-        resizeCanvas()
     }
 
     init {
@@ -124,6 +160,28 @@ class GameFrame : View(), CanvasFitter {
         canvas.prefHeight(canvasPane.height - 20)
         canvas.prefWidth(canvasPane.width - 20)
         canvasPane.add(canvas)
+
+        canvasPane.widthProperty().addListener {_, _, newValue ->
+            println("resizing canvas width to: ${newValue}")
+            canvas.width = newValue.toDouble()
+            setCanvasSize()
+            setScrollbarSizes()
+        }
+
+        canvasPane.heightProperty().addListener { _, _, newValue ->
+            println("resizing canvas height to: ${newValue}")
+            canvas.height = newValue.toDouble()
+            setCanvasSize()
+            setScrollbarSizes()
+        }
+
+        horizontalScroll.valueProperty().addListener { _, _, newValue ->
+            this.blockOffsetX = newValue.toDouble()
+        }
+
+        verticalScroll.valueProperty().addListener { _, _, newValue ->
+            this.blockOffsetY = newValue.toDouble()
+        }
 
         with(canvas) {
             this.setOnScroll {  scrollEvent ->
@@ -143,6 +201,8 @@ class GameFrame : View(), CanvasFitter {
         val timer = object : AnimationTimer() {
             override fun handle(now: Long) {
                 if (ticks == 20) {
+                    canvas.graphicsContext2D.fill = Color.BLACK
+                    canvas.graphicsContext2D.fillRect(0.0,0.0, canvas.width, canvas.height)
                     drawMap(canvas.graphicsContext2D)
                     ticks = 0
                 }
