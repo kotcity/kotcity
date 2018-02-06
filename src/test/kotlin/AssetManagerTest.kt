b2852bc4241f1b212ea73c22f9c9540a2879fc5e
kotcity <kotcity@zoho.com>
2018-02-05 23:12:29 -0500

    fixed imports...

--- src/test/kotlin/AssetManagerTest.kt
+++ src/test/kotlin/AssetManagerTest.kt
@@ -1,10 +1,6 @@
 
-import kotcity.data.CityFileAdapter
-import kotcity.data.MapGenerator
-import kotcity.data.TileType
 import kotcity.data.assets.AssetManager
 import org.junit.jupiter.api.Test
-import java.io.File
 
 class AssetManagerTest {
     @Test
