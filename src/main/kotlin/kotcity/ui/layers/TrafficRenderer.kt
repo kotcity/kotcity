d9cf5a04fad4988afd24f84317cb6b1504c13a6b
kotcity <kotcity@zoho.com>
2018-03-04 16:50:35 -0500

    added traffic flows...

--- src/main/kotlin/kotcity/ui/layers/TrafficRenderer.kt
+++ src/main/kotlin/kotcity/ui/layers/TrafficRenderer.kt
@@ -1,24 +1,104 @@
 package kotcity.ui.layers
 
+import javafx.animation.KeyFrame
+import javafx.animation.Timeline
+import javafx.event.EventHandler
 import javafx.scene.canvas.GraphicsContext
+import javafx.scene.image.Image
 import javafx.scene.paint.Color
+import javafx.util.Duration
 import kotcity.data.BlockCoordinate
 import kotcity.data.CityMap
 import kotcity.data.Road
+import kotcity.memoization.cache
 import kotcity.ui.ResizableCanvas
 import kotcity.ui.map.CityRenderer
 import kotcity.util.Debuggable
 
+
 class TrafficRenderer(private val cityMap: CityMap, private val cityRenderer: CityRenderer, private val trafficCanvas: ResizableCanvas):  Debuggable {
     override var debug: Boolean = true
 
-    private fun highlightBlock(g2d: GraphicsContext, x: Int, y: Int) {
-        g2d.fill = Color(Color.MAGENTA.red, Color.MAGENTA.green, Color.MAGENTA.blue, 0.50)
+    var currentImage = 0
+
+    private val horizontalFilenames = listOf(
+            "file:./assets/animations/light_traffic_1.png",
+            "file:./assets/animations/light_traffic_2.png",
+            "file:./assets/animations/light_traffic_3.png"
+        )
+
+    private val verticalFilenames = listOf(
+            "file:./assets/animations/light_traffic_1_vertical.png",
+            "file:./assets/animations/light_traffic_2_vertical.png",
+            "file:./assets/animations/light_traffic_3_vertical.png"
+    )
+
+    private fun scaledImages(filenames: List<String>, blockSize: Double): List<Image> {
+        return filenames.map {
+            Image(it, blockSize, blockSize, true, true)
+        }
+    }
+
+    init {
+
+        val timelineX = Timeline(KeyFrame(Duration.millis(500.0), EventHandler {
+            currentImage += 1
+            if (currentImage >= horizontalFilenames.count()) {
+                currentImage = 0
+            }
+        }))
+
+        timelineX.cycleCount = Timeline.INDEFINITE
+        timelineX.play()
+    }
+
+    var scaledTrafficCachePair = ::scaledImages.cache()
+    val cachedScaledTrafficImages = scaledTrafficCachePair.second
+
+    private fun drawTrafficImage(g2d: GraphicsContext, coordinate: BlockCoordinate) {
+        // g2d.fill = Color(Color.MAGENTA.red, Color.MAGENTA.green, Color.MAGENTA.blue, 0.50)
         // gotta translate here...
-        val tx = x - cityRenderer.blockOffsetX
-        val ty = y - cityRenderer.blockOffsetY
+        val horizontalImages = cachedScaledTrafficImages(horizontalFilenames, cityRenderer.blockSize())
+        val verticalImages = cachedScaledTrafficImages(verticalFilenames, cityRenderer.blockSize())
+
+        if (horizontalRoad(coordinate)) {
+            drawHorizontal(horizontalImages[currentImage], g2d, coordinate)
+        }
+        if (verticalRoad(coordinate)) {
+            drawVertical(verticalImages[currentImage], g2d, coordinate)
+        }
+
+    }
+
+    private fun verticalRoad(coordinate: BlockCoordinate): Boolean {
+        val top = BlockCoordinate(coordinate.x, coordinate.y - 1)
+        val bottom = BlockCoordinate(coordinate.x, coordinate.y + 1)
+        return (hasRoad(top) || hasRoad(bottom))
+    }
+
+    private fun horizontalRoad(coordinate: BlockCoordinate): Boolean {
+        val left = BlockCoordinate(coordinate.x - 1, coordinate.y)
+        val right = BlockCoordinate(coordinate.x + 1, coordinate.y)
+        return (hasRoad(left) || hasRoad(right))
+    }
+
+    private fun hasRoad(coordinate: BlockCoordinate): Boolean {
+        return cityMap.cachedBuildingsIn(coordinate).any { it.building is Road }
+    }
+
+    private fun drawVertical(image: Image, g2d: GraphicsContext, coordinate: BlockCoordinate) {
+        val tx = coordinate.x - cityRenderer.blockOffsetX
+        val ty = coordinate.y - cityRenderer.blockOffsetY
         val blockSize = cityRenderer.blockSize()
-        g2d.fillRect(tx * blockSize, ty * blockSize, blockSize, blockSize)
+        g2d.drawImage(image, tx * blockSize, ty * blockSize)
+    }
+
+    private fun drawHorizontal(image: Image, g2d: GraphicsContext, coordinate: BlockCoordinate) {
+        // handle horizontal road...
+        val tx = coordinate.x - cityRenderer.blockOffsetX
+        val ty = coordinate.y - cityRenderer.blockOffsetY
+        val blockSize = cityRenderer.blockSize()
+        g2d.drawImage(image, tx * blockSize, ty * blockSize)
     }
 
     fun render() {
@@ -29,10 +109,11 @@ class TrafficRenderer(private val cityMap: CityMap, private val cityRenderer: Ci
         trafficCanvas.graphicsContext2D.fillText("Traffic!", 10.0, 10.0)
 
         visibleBlockRange?.let {visibleBlockRange ->
+
             BlockCoordinate.iterate(visibleBlockRange.first, visibleBlockRange.second) {
                 // see if we got a road under here... AND we got some traffic...
-                if (cityMap.cachedBuildingsIn(it).any{it.building is Road} && cityMap.trafficLayer[it] ?: 0.0 > 0.0) {
-                    highlightBlock(trafficCanvas.graphicsContext2D, it.x, it.y)
+                if (cityMap.cachedBuildingsIn(it).any{it.building is Road} && cityMap.trafficLayer[it] ?: 0.0 > 30.0) {
+                    drawTrafficImage(trafficCanvas.graphicsContext2D, it)
                 }
             }
         }
