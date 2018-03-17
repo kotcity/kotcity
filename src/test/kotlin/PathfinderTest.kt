bde7605736c8f0c008ebd73ba4c586add3ecbaed
kotcity <kotcity@zoho.com>
2018-03-17 17:06:32 -0400

    don't need debug output on contracts now...

--- src/test/kotlin/PathfinderTest.kt
+++ src/test/kotlin/PathfinderTest.kt
@@ -49,7 +49,7 @@ class PathfinderTest {
         }
 
         val contractFulfiller = ContactFulfiller(flatMap)
-        contractFulfiller.debug = true
+        contractFulfiller.debug = false
         contractFulfiller.signContracts(false, maxMillis = 50000)
 
         // now get all industrial zones... they MUST have all labor populated...
@@ -66,7 +66,7 @@ class PathfinderTest {
                 }
             }
         }
-        
+
         val needsLaborRatio = numberWithoutLabor / numberWithLabor
         assertTrue(needsLaborRatio < 0.05, "Expected 95%+ labor fill rate but was $needsLaborRatio")
     }
