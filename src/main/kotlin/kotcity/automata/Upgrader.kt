63e01221e90982d23a4cccf8117e75f20016e8e0
kotcity <kotcity@zoho.com>
2018-03-20 18:38:53 -0400

    added the cheap house...

--- src/main/kotlin/kotcity/automata/Upgrader.kt
+++ src/main/kotlin/kotcity/automata/Upgrader.kt
@@ -5,5 +5,7 @@ import kotcity.data.CityMap
 class Upgrader(val cityMap: CityMap) {
     fun tick() {
         // (maybe) upgrade some buildings...
+        val eligableBuildings = cityMap.locations().filter { it.building.goodwill > 99 }
+        // pick 3 random
     }
 }
