9d4a6971f6516f004d607692929142521abbec00
kotcity <kotcity@zoho.com>
2018-04-02 22:52:48 -0400

    constructor builds a LITTLE more now...

--- src/main/kotlin/kotcity/automata/Constructor.kt
+++ src/main/kotlin/kotcity/automata/Constructor.kt
@@ -32,7 +32,8 @@ class Constructor(val cityMap: CityMap) : Debuggable {
         val zoneTypes = listOf(Zone.INDUSTRIAL, Zone.COMMERCIAL, Zone.RESIDENTIAL)
         zoneTypes.forEach { zoneType ->
 
-            val howManyBuildings = howManyToBuild(zoneType).coerceAtMost(3)
+            val howManyBuildings = howManyToBuild(zoneType).coerceAtMost(5)
+
             debug("According to our calculations we should build $howManyBuildings for $zoneType")
 
             repeat(howManyBuildings, {
