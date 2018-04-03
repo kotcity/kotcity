2f7aafcbea4cde6f5eb3006aafdef8a7c3d7c42f
Sascha Bieberstein <sbi@scireum.de>
2018-04-03 23:46:03 +0200

    Improve codestyle

--- src/main/kotlin/kotcity/data/QuantizedMap.kt
+++ src/main/kotlin/kotcity/data/QuantizedMap.kt
@@ -3,65 +3,37 @@ package kotcity.data
 open class QuantizedMap<T>(private val quantize: Int = 4) {
     protected var map: MutableMap<BlockCoordinate, T> = mutableMapOf()
 
-    fun put(blockCoordinate: BlockCoordinate, value: T) {
-        map[quantizeBlockCoordinate(blockCoordinate)] = value
-    }
-
     operator fun set(blockCoordinate: BlockCoordinate, value: T) {
         map[quantizeBlockCoordinate(blockCoordinate)] = value
-    }
-
-    fun clear() {
-        map.clear()
-    }
-
-    fun count(): Int {
-        return map.count()
-    }
-
-    fun entries(): MutableSet<MutableMap.MutableEntry<BlockCoordinate, T>> {
-        return map.entries
     }
 
     operator fun get(blockCoordinate: BlockCoordinate): T? {
         return map[quantizeBlockCoordinate(blockCoordinate)]
     }
 
-    fun remove(blockCoordinate: BlockCoordinate) {
-        map.remove(blockCoordinate)
-    }
+    fun clear() = map.clear()
+
+    fun count() = map.count()
+
+    fun keys() = map.keys
+
+    fun entries() = map.entries
+
+    fun remove(blockCoordinate: BlockCoordinate) = map.remove(blockCoordinate)
 
     private fun quantizeBlockCoordinate(blockCoordinate: BlockCoordinate) =
-            BlockCoordinate(blockCoordinate.x / this.quantize, blockCoordinate.y / this.quantize)
-
-    fun map(function: (Map.Entry<BlockCoordinate, T>) -> Any): List<Any> {
-        return map.map(function)
-    }
-
-    fun maxBy(function: (Map.Entry<BlockCoordinate, T>) -> Double): Map.Entry<BlockCoordinate, T>? {
-        return map.maxBy { function(it) }
-    }
-
-    fun clone(): QuantizedMap<T> {
-        val newMap = QuantizedMap<T>(quantize = this.quantize)
-        map.forEach { t, u ->
-            newMap.put(t, u)
-        }
-        return newMap
-    }
+        BlockCoordinate(blockCoordinate.x / this.quantize, blockCoordinate.y / this.quantize)
 
     fun unquantized(coordinate: BlockCoordinate): List<BlockCoordinate> {
-        val xRange = (coordinate.x .. coordinate.x+quantize)
-        val yRange = (coordinate.y .. coordinate.y+quantize)
+        val xRange = (coordinate.x..coordinate.x + quantize)
+        val yRange = (coordinate.y..coordinate.y + quantize)
 
         val blocks = mutableListOf<BlockCoordinate>()
         xRange.forEach { x -> yRange.mapTo(blocks) { BlockCoordinate(x, it) } }
         return blocks.toList()
     }
 
-    fun keys(): MutableSet<BlockCoordinate> {
-        return map.keys
-    }
+    fun map(function: (Map.Entry<BlockCoordinate, T>) -> Any): List<Any> = map.map(function)
 
     fun forEach(lambda: (t: BlockCoordinate, T) -> Unit) {
         map.forEach { t, u ->
@@ -69,4 +41,11 @@ open class QuantizedMap<T>(private val quantize: Int = 4) {
         }
     }
 
+    fun clone(): QuantizedMap<T> {
+        val newMap = QuantizedMap<T>(quantize = this.quantize)
+        map.forEach { key, value ->
+            newMap[key] = value
+        }
+        return newMap
+    }
 }
